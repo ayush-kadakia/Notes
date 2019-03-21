8abeb1f7bcfdd8793318d31b3394ce6e19b507ea
Ayush Kadakia <ayush.kadakia@live.com>
2019-03-21 01:19:46 -0400

    Added Auth

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,34 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func loginTapped(_ sender: UIButton) {
+        
+        //Gets the Auth UI Object
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else{
+            return
+        }
+        
+        //Assigns Delegate
+        authUI?.delegate = self
+        
+        //Get the ref to the auth UI VC
+        let authViewController = authUI!.authViewController()
+        
+        //Show it
+        present(authViewController, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: FUIAuthDelegate{
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        
+        if(error != nil){
+            return
+        }
+        
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
+}
